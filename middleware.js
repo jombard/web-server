80f079fa6aede9e3f785554a48f5568cea7fe9df
Jon Lombard <jombard@hotmail.com>
2015-11-26 19:42:16 +0000

    Update port for heroku

--- middleware.js
+++ middleware.js
@@ -1,12 +1,10 @@
-
-
 var middleware = {
 	requireAuthentication: function (req, res, next) {
 		console.log('private route hit!');
 		next();
 	},
 	logger: function (req, res, next) {
-		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.OriginalURL);
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
 		next();
 	}
 };
